3a94ee4058887caff8dca00b97131d9e6c6175b2
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-21 13:44:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,4 +30,25 @@ function showCommits(el) {
   const owner = el.dataset.owner
   const repo = el.dataset.repository
   const url = "https://api.github.com/repos/" + owner + "/" + repo + "/commits";
+
+  $.get(url, function(data) {
+    console.log(data.items);
+
+    const repos = data.items;
+    const repoList = "<ul>" + repos.map(r => {
+      return (`
+        <li>
+        <h2><a href="${r.html_url}">${r.name}</a></h2>
+        <p>Description: ${r.description}</p>
+        <p>Created by: ${r.owner.login}</p>
+        <a href="#" onclick="showCommits(this)" data-repository="${r.name}" data-owner="${r.owner.login}">Show Commits</a>
+        <p><img src="${r.owner.avatar_url}" height="45" width="45"></p>
+        </li>
+        `)
+    }).join('') + "</ul>"
+    document.getElementById("results").innerHTML = repoList
+  }).fail(function() {
+    displayError();
+  });
+
 }
