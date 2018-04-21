c0e35fb0f9f9e95e1cbf80a4f9d6e5a6d09b0b66
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-21 13:38:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,8 @@ $(document).ready(function (){
 function searchRepositories() {
   const input = $("#searchTerms")[0].value;
   const url = "https://api.github.com/search/repositories?q=" + input;
+
+  $.get(url, function(data) {
+    console.log(data.items);
+  })
 }
