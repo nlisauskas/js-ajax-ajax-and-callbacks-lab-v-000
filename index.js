bcd89daa740bd782f45daefbcc047e72d6b7925f
Nick Lisauskas <nlisausk@alumni.nd.edu>
2018-04-21 13:42:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,8 +15,13 @@ function searchRepositories() {
         <h2><a href="${r.html_url}">${r.name}</a></h2>
         <p>Description: ${r.description}</p>
         <p>Created by: ${r.owner.login}</p>
-        <a href="#" onclick="showCommits(this)" data-repository="${r.name}"
+        <a href="#" onclick="showCommits(this)" data-repository="${r.name}" data-owner="${r.owner.login}">Show Commits</a>
+        <p><img src="${r.owner.avatar_url}" height="45" width="45"></p>
+        </li>
         `)
-    })
+    }).join('') + "</ul>"
+    document.getElementById("results").innerHTML = repoList
+  }).fail(function() {
+    displayError();
   })
 }
